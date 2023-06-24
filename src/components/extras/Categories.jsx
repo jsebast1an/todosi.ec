f462ba80f21e591d7201a93185bff42cdc632a5b
jsebastianbox <juanse.lopez@bussbox.com>
2023-06-24 18:42:36 -0300

    poniendo imagenes de categorias listo

--- src/components/extras/Categories.jsx
+++ src/components/extras/Categories.jsx
@@ -1,26 +1,30 @@
-
+import imgBelleza from '../../imgs/imagen-belleza.jpg'
+import imgJuguetes from '../../imgs/imagen-juguetes.jpg'
+import imgCocina from '../../imgs/imagen-cocina.jpg'
+import imgSeguridad from '../../imgs/imagen-seguridad.jpg'
 
 /* css */
 import './extras.css'
 
 // Array de objetos con categorías y sus imágenes
 const categorias = [
-  { nombre: 'Incluyeme', imagen: '../imagen-incluyeme.jpg' },
-  { nombre: 'Belleza', imagen: 'ruta/imagen-belleza.jpg' },
-  { nombre: 'Seguridad', imagen: 'ruta/imagen-seguridad.jpg' },
-  { nombre: 'Mascotas', imagen: 'ruta/imagen-mascotas.jpg' },
-  { nombre: 'Juguetes', imagen: 'ruta/imagen-juguetes.jpg' }
+  { nombre: 'Belleza', imagen: imgBelleza },
+  { nombre: 'Seguridad', imagen: imgSeguridad },
+  { nombre: 'Cocina', imagen: imgCocina },
+  { nombre: 'Juguetes', imagen: imgJuguetes }
 ]
 
 const CategoriasComponente = () => {
     return (
         <div className="categoria_container">
-            {categorias.map((categoria, index) => (
-                <div key={index} className='categoria'>
-                    <h2>{categoria.nombre}</h2>
-                    <img src={categoria.imagen} alt={categoria.nombre} />
-                </div>
-            ))}
+            {
+                categorias.map((categoria, index) => (
+                    <div key={index} className='categoria'>
+                        <h2>{categoria.nombre}</h2>
+                        <img src={categoria.imagen} alt={categoria.nombre} />
+                    </div>
+                ))
+            }
         </div>
     )
 }
