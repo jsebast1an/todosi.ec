51a6e58fe5a3e6d369194f8a17345f281a11ce4f
jsebastianbox <juanse.lopez@bussbox.com>
2023-09-18 11:38:10 -0300

    organizando fotos, categorias y precios

--- src/components/SecondSection.jsx
+++ src/components/SecondSection.jsx
@@ -1,43 +1,74 @@
+import * as Bootstrap from 'react-bootstrap'
 import {Carousel} from 'react-responsive-carousel';
+import { useState } from 'react';
+
 
 import './css/secondSection.css'; // Asegúrate de tener un archivo CSS para estilizar la galería
 import 'react-responsive-carousel/lib/styles/carousel.min.css'; // Importa los estilos del carrusel
 
+    
+let imageNames = [
+    //promo
+    { name: 'DESCUENTO-STEP-ONE.jpg', category: 'promo', category_2: ''},
+    { name: 'promo-step-one-2.jpg', category: 'promo', category_2: ''},
+    { name: 'PROMO-COCINA.jpg', category: 'promo', category_2: ''},
+
+    //belleza
+    { name: 'post-step-one.jpg', category: 'ropa', category_2: ''},
+    { name: 'CEPILLO-ALISADOR.jpg', category: 'belleza', category_2: ''},
+    { name: 'DEPILADORA-DE-CRISTAL.jpg', category: 'belleza', category_2: ''},
+    { name: 'MAQUINA-CORTAPELO.jpg', category: 'belleza', category_2: 'tecnología'}, //cambiar precio 12.99
+    { name: 'CUBRE-PEZON.jpg', category: 'belleza', category_2: ''},
+    
+    //hogar
+    { name: 'CEPILLO-MAGIC-BRUSH.jpg', category: 'hogar', category_2: ''},
+    { name: 'AFILADOR-POST.jpg', category: 'hogar', category_2: 'cocina'},
+    { name: 'LIMPIADOR-DE-VIDRIO-MAGNETICO-POST.jpg', category: 'hogar', category_2: ''}, //cambiar precio 12.99
+    { name: 'mini-laser.jpg', category: 'hogar', category_2: 'tecnología'},
+    { name: 'MOLINILLO-DE-CAFE-POST.jpg', category: 'hogar', category_2: ''},
+    { name: 'ORGANIZADOR-PARA-REFFRI.jpg', category: 'hogar', category_2: ''},
+    { name: 'POST-15-DE-ABRIL.jpg', category: 'hogar', category_2: ''},
+    { name: 'POST-PICAPORTE.jpg', category: 'hogar', category_2: ''}, //cambiar a 19.99
+    { name: 'PURIFICADOR-DE-AGUA-SWS.jpg', category: 'hogar', category_2: ''}, //cambiar a 10.99
+    
+    //mascotas
+    { name: 'GUANTE-MASCOTA.jpg', category: 'mascotas', category_2: ''},
+    { name: 'LIMPIA-PELUSAS.jpg', category: 'mascotas', category_2: ''},
+    { name: 'REMOVEDOR-DE-PELO.jpg', category: 'mascotas', category_2: 'hogar'},
+
+    
+    //celular
+    { name: 'promo-celular-portada.jpg', category: 'celular', category_2: ''},
+    { name: 'foco-parlante.jpg', category: 'celular', category_2: ''},
+    { name: 'protector-de-celular-waterproof.jpg', category: 'celular', category_2: 'hogar'},
+    { name: 'microfono.jpg', category: 'celular', category_2: ''}, //cambiar a 14.99
+    { name: 'POST-11-DE-ABRIL.jpg', category: 'celular', category_2: ''},
+    { name: 'audifonos-bluetooh.jpg', category: 'celular', category_2: ''}, //cambiar
+
+    //seguridad
+    { name: 'MINI-CAMARA-ESPIA-POST.jpg', category: 'seguridad', category_2: 'hogar'}, //cambiar a 14.99
+    
+    //niños
+    { name: 'MINI-CONSOLA-RETRO.jpg', category: 'niños y niñas', category_2: 'juguetes'}, //cambiar a 19.99
+
+    //tecnología
+    { name: 'pantalla-lcd.jpg', category: 'tecnología', category_2: 'niños y niñas'},
+    { name: 'post-gps', category: 'tecnología', category_2: 'hogar'},
+
+    //juguetes
+    { name: 'PISTOLA-HIDROGEL.jpg', category: 'juguetes', category_2: 'niños y niñas'},
+
+];
 
 function SecondSection() {
-    // Arreglo con los nombres de tus imágenes
-    let imageNames = [
-        'CEPILLO-ALISADOR.jpg',
-        // 'DESCUENTO-STEP-ONE.jpg',
-        'CEPILLO-MAGIC-BRUSH.jpg',
-        'GUANTE-MASCOTA.jpg',
-        'PROMO-COCINA.jpg',
-        'promo-celular-portada.jpg',
-        'LIMPIA-PELUSAS.jpg',
-        'AFILADOR-POST.jpg',
-        'DEPILADORA-DE-CRISTAL.jpg',
-        'foco-parlante.jpg',
-        'MAQUINA-CORTAPELO.jpg',
-        'LIMPIADOR-DE-VIDRIO-MAGNETICO-POST.jpg',
-        'microfono.jpg',
-        'MINI-CAMARA-ESPIA-POST.jpg',
-        'MINI-CONSOLA-RETRO.jpg',
-        'mini-laser.jpg',
-        'MOLINILLO-DE-CAFE-POST.jpg',
-        'ORGANIZADOR-PARA-REFFRI.jpg',
-        'pantalla-lcd.jpg',
-        'PISTOLA-HIDROGEL.jpg',
-        'POST-11-DE-ABRIL.jpg',
-        'POST-15-DE-ABRIL.jpg',
-        'post-18-de-abril.jpg',
-        'POST-PICAPORTE.jpg',
 
-        //imgs estilo viejo
-        'CUBRE-PEZON.jpg',
-        'audifonos-bluetooh.jpg',
-        // Agrega el resto de los nombres de tus imágenes aquí
-    ];
+    let [selectedCategory, setSelectedCategory] = useState('all');
+ 
+    let handleCategoryChange = (category) => {
+        setSelectedCategory(category)
+    }
 
+    const filteredImages = selectedCategory === 'all' ? imageNames : imageNames.filter((image) => image.category === selectedCategory || image.category_2 === selectedCategory);
 
     return (
         <div id="secondSection" className="gallery">
@@ -50,7 +81,22 @@ function SecondSection() {
             </div>
 
             <h4>Nuestros productos</h4>
-
+            
+            <Bootstrap.Form.Select 
+                    onChange={(e) => handleCategoryChange(e.target.value)} 
+                    style={{ width: '320px', margin: '1rem auto'}}>
+                <option value="all">Selecciona una opción</option>
+                <option value="all">Todos los productos</option>
+                <option value="promo">Promociones Septiembre</option>
+                <option value="belleza">Belleza</option>
+                <option value="hogar">Hogar</option>
+                <option value="mascotas">Mascotas</option>
+                <option value="celular">Accesorios para celular</option>
+                <option value="seguridad">Seguridad</option>
+                <option value="niños y niñas">Niños y niñas</option>
+                <option value="tecnología">Tecnología</option>
+                <option value="juguetes">Juguetes</option>
+            </Bootstrap.Form.Select>
 
             <Carousel 
                 id="productsCarousel"
@@ -62,10 +108,10 @@ function SecondSection() {
                 centerSlidePercentage={100}
                 // width={'330px'}
                 >
-                    {imageNames.map((imageName, index) => (
+                    {filteredImages.map((imageName, index) => (
                     <div key={index} className="gallery-item">
                         <img
-                        src={`${process.env.PUBLIC_URL}/imgs/products/${imageName}`}
+                        src={`${process.env.PUBLIC_URL}/imgs/products/${imageName.name}`}
                         alt={`Imagen ${index + 1}`}
                         width={350}
                         />
@@ -73,20 +119,8 @@ function SecondSection() {
                     ))}
             </Carousel>
         </div>
-    );
-      
+    )      
 
-    // return (
-    //     <div id='secondSection' className="gallery">
-    //         {imageNames.map((imageName, index) => (
-    //             <div key={index} className="gallery-item">
-    //                 <img src={`${process.env.PUBLIC_URL}/imgs/products/${imageName}`} 
-    //                     alt={`Imagen ${index + 1}`}
-    //                     width={350} />
-    //             </div>
-    //         ))}
-    //     </div>
-    // );
 }
 
 export default SecondSection
